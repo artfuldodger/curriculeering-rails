6d8057b6037a4df1d1c3132c635b58fd656071b0
Jon Evans <jle@foraker.com>
2015-02-23 20:47:19 -0700

    Add ability to update lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,20 @@
 class LessonsController < ApplicationController
   def show
-    render json: Lesson.find(params[:id])
+    render json: lesson
+  end
+
+  def update
+    lesson.update_attributes(lesson_params)
+    render json: lesson
+  end
+
+  private
+
+  def lesson
+    Lesson.find(params[:id])
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :curriculums, only: [:index, :show, :update]
-  resources :lessons, only: [:show]
+  resources :lessons, only: [:show, :update]
   resources :instructors, only: [:index, :show]
 end
